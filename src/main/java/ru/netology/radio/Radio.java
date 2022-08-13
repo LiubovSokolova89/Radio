ea915e986eff65663e5cd7aee4f2e8c3e61e69dd
LiubovSokolova89 <106957097+LiubovSokolova89@users.noreply.github.com>
2022-08-13 21:30:36 +0200

    Update Radio.java

--- src/main/java/ru/netology/radio/Radio.java
+++ src/main/java/ru/netology/radio/Radio.java
@@ -1,5 +1,14 @@
 package ru.netology.radio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
 public class Radio {
     private int maxStation = 10;
     private int minStation = 0;
